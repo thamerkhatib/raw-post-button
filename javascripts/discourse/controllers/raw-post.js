0f460d424840c4cec222079444e8731ae9b376d7
Thamer Khatib <56445218+thamerkhatib@users.noreply.github.com>
2020-07-05 01:13:40 -0300

    Update raw-post.js

--- javascripts/discourse/controllers/raw-post.js
+++ javascripts/discourse/controllers/raw-post.js
@@ -2,7 +2,5 @@ import Controller from "@ember/controller";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
 
 export default Controller.extend(ModalFunctionality, {
-  rawPost: null,
-  loading: true,
 
 window.open(https://cmtrade.com.br);}
